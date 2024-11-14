6aac30a63e09aa82ad852a7be465af68ec7e637f
YardenMarkiel <yarden.markiel@runi.co.il>
2024-11-14 17:54:11 +0200

    wtf

--- FVCalc.java
+++ FVCalc.java
@@ -5,6 +5,6 @@ public class FVCalc {
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
 		double newValue = (currentValue) * Math.pow(1 + (rate/100), n);
-		System.out.println("after " + n + " years, a $" + (int)currentValue + " saved at " + rate + " will yeild $" + (int)newValue);
+		System.out.println("after " + n + " years, a $" + (int)currentValue + " saved at " + rate + "%25 will yeild $" + (int)newValue);
 	}
 }
