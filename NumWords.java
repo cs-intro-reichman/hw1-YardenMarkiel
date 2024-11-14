bd030696dfd6ab6ceb5ff5fe7a3d5b5b5b227c08
YardenMarkiel <yarden.markiel@runi.co.il>
2024-11-14 14:47:23 +0200

    fixed numwords

--- NumWords.java
+++ NumWords.java
@@ -1,11 +1,11 @@
 
 public class NumWords {
 	public static void main(String args[]) {
-		int number = Integer.parseInt(args[0]);
+		int number = Integer.parseInt(args[0]); //supose to seperate 3 3
 		int hundreds = number/100;
 		int tens = (number-(hundreds*100)) / 10;
 		int units = (number - ((hundreds*100) + (tens * 10)));
-		System.out.println(hundreds + " " + tens + " " + units);
+		System.out.println(hundreds + " hundreds " + tens + " tens, and " + units + " ones");
 
 	}
 }
