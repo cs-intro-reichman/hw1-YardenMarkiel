a9e4283311cdb5cf01871d5e947ad0390fdb0b17
YardenMarkiel <yarden.markiel@runi.co.il>
2024-11-14 17:05:56 +0200

    fixed bug ,

--- Bill3.java
+++ Bill3.java
@@ -1,13 +1,12 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
 		Double price = Double.parseDouble(args[3]);
 		Double splitPrice = price / 3;
 		splitPrice = Math.ceil(splitPrice);
-		System.out.println("Dear " + name3  + ", "+ name2 + " and " + name1 + ": pay " + splitPrice + " Shekels each");
+		System.out.println("Dear " + name3  + ", "+ name2 + ", and " + name1 + ": pay " + splitPrice + " Shekels each");
 	}
 }
